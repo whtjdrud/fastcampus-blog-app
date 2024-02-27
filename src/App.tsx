ab7312c56bad93e9ddac36ae63384ce8b3ff1bc7
kkambo-frontend <whtjdrud800@gmail.com>
2024-02-27 14:04:30 +0900

    :sparkles: react-router-dom 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { Route, Routes, Navigate, Link } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <ul>
+          <li>
+            <Link to='/'> Home</Link>
+          </li>
+          <li>
+            <Link to='/posts'> Post Link</Link>
+          </li>
+          <li>
+            <Link to='/posts/:id'> Post Detail</Link>
+          </li>
+          <li>
+            <Link to='/posts/new'> Post New</Link>
+          </li>
+          <li>
+            <Link to='/posts/edit/:id'> Post edit</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route path='/' element={<h1>HomePage</h1>} />
+        <Route path='/posts' element={<h1>Post List Page</h1>} />
+        <Route path='/posts/:id' element={<h1>Post Detail Page</h1>} />
+        <Route path='/posts/new' element={<h1>Post New</h1>} />
+        <Route path='/posts/edit/:id' element={<h1>Post edit</h1>} />
+        <Route path='/profile' element={<h1>Profile</h1>} />
+        <Route path='*' element={<Navigate replace to='/' />} />
+      </Routes>
+    </>
   );
 }
 
