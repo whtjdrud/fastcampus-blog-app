7798d165de8e718a25a06b21fa6fb20b4d9289ec
kkambo-frontend <whtjdrud800@gmail.com>
2024-02-28 13:06:06 +0900

    :sparkles: Carousel 컴포넌트 작업

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 import PostList from 'components/PostList';
+import Carousel from 'components/Carousel';
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Carousel />
       <PostList />
       <Footer />
     </div>
